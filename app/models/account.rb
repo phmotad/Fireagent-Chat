0c8ec8d7006762adff54eff347c91b6e0e830ce4
phmotad <phmotad@gmail.com>
2025-11-28 13:50:30 -0300

    fix: add missing ai_agents association to Account model
    - Resolves NoMethodError: undefined method 'ai_agents' for Account
    - Account model now has has_many :ai_agents association
    - Fixes 500 error when accessing /api/v1/accounts/:id/ai_agents

--- app/models/account.rb
+++ app/models/account.rb
@@ -59,6 +59,7 @@ class Account < ApplicationRecord
   has_many :account_users, dependent: :destroy_async
   has_many :agent_bot_inboxes, dependent: :destroy_async
   has_many :agent_bots, dependent: :destroy_async
+  has_many :ai_agents, dependent: :destroy_async
   has_many :api_channels, dependent: :destroy_async, class_name: '::Channel::Api'
   has_many :articles, dependent: :destroy_async, class_name: '::Article'
   has_many :assignment_policies, dependent: :destroy_async
